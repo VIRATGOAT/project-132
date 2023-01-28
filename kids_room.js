6f22970f16590bf7e68329879aecbf1142207b78
VIRATGOAT <121668275+VIRATGOAT@users.noreply.github.com>
2023-01-28 14:51:23 +0530

    Add files via upload

---
+++ kids_room.js
@@ -0,0 +1,34 @@
+objectDetector="";
+img="";
+status="";
+objects=[];
+function setup(){
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function preload(){
+   img = loadImage("kids_room.jpg");
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+} 
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function logout(){
+    window.location="index.html";
+  }
